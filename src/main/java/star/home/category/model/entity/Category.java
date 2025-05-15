4b7507b809ea1ceb27bba5c22d8a9da72c66a219
rdme0 <we6610@gmail.com>
2025-05-15 21:47:45 +0900

    :recycle: refactor(Category 엔티티): id -> name 으로 리팩터링
    이유: 코드의 가독성이 우선순위 1순위이고, category 테이블의 카테고리 데이터는 불변이기 때문이다.

--- src/main/java/star/home/category/model/entity/Category.java
+++ src/main/java/star/home/category/model/entity/Category.java
@@ -2,10 +2,9 @@ package star.home.category.model.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import star.common.entity.BaseEntity;
@@ -15,9 +14,10 @@ import star.common.entity.BaseEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 20, unique = true)
     private String name;
+
+    @Builder Category(String name) {
+        this.name = name.toLowerCase();
+    }
 }
