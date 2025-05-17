9da32a7134a8caa0f475943659bc4907b9d2b32e
rdme0 <we6610@gmail.com>
2025-05-17 15:57:04 +0900

    :goal_net: refactor: 예외처리 상세화

--- src/main/java/star/home/board/exception/AlreadyHeartedException.java
+++ src/main/java/star/home/board/exception/AlreadyHeartedException.java
@@ -1,8 +1,8 @@
 package star.home.board.exception;
 
-import star.common.exception.ClientException;
+import star.common.exception.client.Client409Exception;
 
-public class AlreadyHeartedException extends ClientException {
+public class AlreadyHeartedException extends Client409Exception {
     private static final String ERROR_MESSAGE = "이미 좋아요를 하였습니다.";
 
     public AlreadyHeartedException() {
