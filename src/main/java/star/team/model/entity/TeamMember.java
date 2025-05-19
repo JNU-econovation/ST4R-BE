4af8e84465fb29e6d3b1925711e17af151d9a3cf
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-05-19 13:26:02 +0900

    :sparkles: feat: Team 관련 엔티티 정의

---
+++ src/main/java/star/team/model/entity/TeamMember.java
@@ -0,0 +1,25 @@
+package star.team.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import star.member.model.entity.Member;
+
+@Entity
+public class TeamMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Team team;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Member member;
+}
