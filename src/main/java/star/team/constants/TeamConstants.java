6e8a9a68af8c0af3f9b210a37c8393e76bc60daf
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-05-19 14:55:01 +0900

    :recycle: refactor: 공통 상수 분리

--- src/main/java/star/team/constants/TeamConstants.java
+++ src/main/java/star/team/constants/TeamConstants.java
@@ -4,11 +4,11 @@ public final class TeamConstants {
     public static final int PASSWORD_MIN_LENGTH = 4;
     public static final int PASSWORD_MAX_LENGTH = 32;
 
-    public static final Integer PARTICIPANT_MIN_CAPACITY = 1;
-    public static final Integer PARTICIPANT_MAX_CAPACITY = 30;
+    public static final int PARTICIPANT_MIN_CAPACITY = 1;
+    public static final int PARTICIPANT_MAX_CAPACITY = 30;
 
-    public static final Integer NAME_MAX_LENGTH = 30;
-    public static final Integer NAME_MIN_LENGTH = 2;
+    public static final int NAME_MAX_LENGTH = 30;
+    public static final int NAME_MIN_LENGTH = 2;
 
-    public static final Integer DESCRIPTION_MAX_LENGTH = 1000;
+    public static final int DESCRIPTION_MAX_LENGTH = 1000;
 }
