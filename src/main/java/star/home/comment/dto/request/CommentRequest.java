aec1cbdcad3b1d88b0964b55b81aabef889aa2c2
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-05-21 23:57:18 +0900

    :recycle: refactor: 공통 상수 분리

--- src/main/java/star/home/comment/dto/request/CommentRequest.java
+++ src/main/java/star/home/comment/dto/request/CommentRequest.java
@@ -1,12 +1,15 @@
 package star.home.comment.dto.request;
 
+import static star.home.comment.constants.CommentConstants.COMMENT_CONTENT_MAX_LENGTH;
+import static star.home.comment.constants.CommentConstants.COMMENT_CONTENT_MIN_LENGTH;
+
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public record CommentRequest(
         @NotBlank(message = "댓글이 공백일 수 없습니다.")
-        @Size(min = 2, max = 300, message = "댓글의 길이는 최소 {min}자, 최대 {max}자여야 합니다.")
+        @Size(min = COMMENT_CONTENT_MIN_LENGTH, max = COMMENT_CONTENT_MAX_LENGTH, message = "댓글의 길이는 최소 {min}자, 최대 {max}자여야 합니다.")
         String content,
 
         @Nullable
