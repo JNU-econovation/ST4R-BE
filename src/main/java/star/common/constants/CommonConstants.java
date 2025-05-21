cc36de4d120b6a524d5199029b7ac24b6c3453da
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-05-21 21:56:46 +0900

    :recycle: refactor: 성능 & 구조적 댓글 조회 리팩터링
    1. 기존 API는 게시글 조회 시 게시글과 댓글을 한번에 조회한다.
    2. 댓글 조회 시 List 구조를 사용하여 탐색하는 불필요한 로직이 시간복잡도를 O(n^2)으로 만든다.
    3. 이는 성능 이슈가 예상되었다.
    4. 따라서 Pageable 객체로 댓글 일부분을 조회하고
    5. List -> Map 리팩터링으로 시간복잡도 O(n) 향상시키는 리팩터링을 하였다.

--- src/main/java/star/common/constants/CommonConstants.java
+++ src/main/java/star/common/constants/CommonConstants.java
@@ -4,4 +4,5 @@ public final class CommonConstants {
     public static final String CRITICAL_ERROR_MESSAGE = "서버에서 예상치 못한 에러가 발생하였습니다.";
     public static final int OPTIMISTIC_ATTEMPT_COUNT = 10;
     public static final int MAX_IMAGE_COUNT = 10;
+    public static final long ANONYMOUS_MEMBER_ID = -12345678L;
 }
