e200af84c302a38d5a8b913b512ddd2f68048346
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-05-29 21:15:54 +0900

    :recycle: refactor: 커스텀 예외 공통 클래스로 분리

--- src/main/java/star/common/exception/client/AlreadyHeartedException.java
+++ src/main/java/star/common/exception/client/AlreadyHeartedException.java
@@ -1,6 +1,4 @@
-package star.home.board.exception;
-
-import star.common.exception.client.Client409Exception;
+package star.common.exception.client;
 
 public class AlreadyHeartedException extends Client409Exception {
     private static final String ERROR_MESSAGE = "이미 좋아요를 하였습니다.";
