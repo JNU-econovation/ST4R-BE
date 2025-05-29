0e63c459b452102a108cef27c24c7228c99ce465
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-05-29 21:57:05 +0900

    :recycle: refactor: 상속 클래스 변경

--- src/main/java/star/team/repository/TeamMemberRepository.java
+++ src/main/java/star/team/repository/TeamMemberRepository.java
@@ -1,11 +1,11 @@
 package star.team.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import star.team.model.entity.TeamMember;
 
 @Repository
-public interface TeamMemberRepository extends CrudRepository<TeamMember, Long> {
+public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
 
     void deleteTeamMembersByTeamId(Long teamId);
 }
