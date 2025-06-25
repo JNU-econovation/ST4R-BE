49e48b308b02057484c44cd647854e2e5817c123
rdme0 <we6610@gmail.com>
2025-06-26 00:38:50 +0900

    :sparkles: feat: 채팅 서비스 정의

--- src/main/java/star/team/chat/dto/response/ChatResponse.java
+++ src/main/java/star/team/chat/dto/response/ChatResponse.java
@@ -1,9 +1,22 @@
 package star.team.chat.dto.response;
 
 import java.time.OffsetDateTime;
+import lombok.Builder;
+import star.common.util.CommonTimeUtils;
+import star.team.chat.model.entity.Chat;
 
+@Builder
 public record ChatResponse(
-        Long chatId, Long memberId, String nickName, OffsetDateTime chattedAt, String message
+        Long chatId, Long memberId, String email, OffsetDateTime chattedAt, String message
 ) {
+    public static ChatResponse from(Chat chat) {
+        return ChatResponse.builder()
+                .chatId(chat.getId())
+                .memberId(chat.getTeamMember().getMember().getId())
+                .email(chat.getTeamMember().getMember().getEmail().value())
+                .chattedAt(CommonTimeUtils.convertLocalDateTimeToOffsetDateTime(chat.getCreatedAt()))
+                .message(chat.getMessage().value())
+                .build();
 
+    }
 }
