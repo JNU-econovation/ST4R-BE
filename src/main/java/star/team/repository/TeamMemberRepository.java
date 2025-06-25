370bdc5216e30e73a4472e1b93cfabdfdf7ac693
rdme0 <we6610@gmail.com>
2025-06-26 01:07:20 +0900

    :fire: remove: 필요없는 코드 제거

--- src/main/java/star/team/repository/TeamMemberRepository.java
+++ src/main/java/star/team/repository/TeamMemberRepository.java
@@ -1,6 +1,5 @@
 package star.team.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import star.team.model.entity.TeamMember;
