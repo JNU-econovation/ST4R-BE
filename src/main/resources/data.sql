513ea74bd2fa8042810cc8c0f1be029735bf1e7e
rdme0 <we6610@gmail.com>
2025-07-02 00:41:06 +0900

    :card_file_box: remove: hibernate spatial 제거

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,3 @@
-CREATE ALIAS IF NOT EXISTS H2GIS_SPATIAL FOR 'org.h2gis.functions.factory.H2GISFunctions.load';
-CALL H2GIS_SPATIAL();
-
 INSERT INTO category (created_at, updated_at, name)
 VALUES (NOW(), NOW(), 'SPOT'),
        (NOW(), NOW(), 'GENERAL'),
