0ef48bb6cf6d979c4c6b2a26d3024443538125df
rdme0 <we6610@gmail.com>
2025-07-02 00:42:18 +0900

    :bug: fix: NPE 해결

--- src/main/java/star/common/model/vo/Marker.java
+++ src/main/java/star/common/model/vo/Marker.java
@@ -2,6 +2,7 @@ package star.common.model.vo;
 
 import jakarta.annotation.Nullable;
 import jakarta.persistence.Embeddable;
+import lombok.Builder;
 
 @Embeddable
 public record Marker(
@@ -14,6 +15,7 @@ public record Marker(
     private static final int LOCATION_NAME_MAX_LENGTH = 100;
     private static final int ROAD_ADDRESS_MAX_LENGTH = 200;
 
+    @Builder
     public Marker {
         validate(latitude, longitude, locationName, roadAddress);
     }
@@ -26,7 +28,7 @@ public record Marker(
                 marker.roadAddress());
     }
 
-    private void validate(Double latitude, Double longitude, String locationName,
+    public static void validate(Double latitude, Double longitude, String locationName,
             String roadAddress) {
         if (latitude == null) {
             throw new IllegalArgumentException("위도(latitude)를 입력해주세요.");
@@ -41,7 +43,7 @@ public record Marker(
         if (longitude < -180 || longitude > 180) {
             throw new IllegalArgumentException("경도는 -180도 이상 180도 이하로 입력해주세요.");
         }
-        if (locationName.length() > LOCATION_NAME_MAX_LENGTH) {
+        if (locationName != null && locationName.length() > LOCATION_NAME_MAX_LENGTH) {
             throw new IllegalArgumentException(
                     "장소 이름은 최대 %d자까지 입력할 수 있습니다.".formatted(LOCATION_NAME_MAX_LENGTH));
         }
