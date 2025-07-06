2607347b46f6c3f4703b0019eeef04bb96e4a8d7
rdme0 <we6610@gmail.com>
2025-07-06 17:34:41 +0900

    :sparkles: feat(dto): 팀 구성원 조회 기능 추가

---
+++ src/main/java/star/team/dto/response/TeamMembersResponse.java
@@ -0,0 +1,23 @@
+package star.team.dto.response;
+
+import lombok.Builder;
+import star.member.dto.MemberInfoDTO;
+
+@Builder
+public record TeamMembersResponse(
+        Long id,
+        String imageUrl,
+        String nickname,
+        Boolean isLeader,
+        Boolean isMe
+) {
+    public static TeamMembersResponse from(MemberInfoDTO memberInfoDTO, Boolean isLeader, Boolean isMe) {
+        return TeamMembersResponse.builder()
+                .id(memberInfoDTO.id())
+                .nickname(memberInfoDTO.email().getValue())
+                .imageUrl(memberInfoDTO.profileImageUrl())
+                .isLeader(isLeader)
+                .isMe(isMe)
+                .build();
+    }
+}
