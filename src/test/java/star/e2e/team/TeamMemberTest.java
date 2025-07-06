b11b8afb5e3d8d0675360e71e32e488d66d29ea1
rdme0 <we6610@gmail.com>
2025-07-06 17:51:05 +0900

    :white_check_mark: test: 모임-멤버 E2E 테스트 추가

---
+++ src/test/java/star/e2e/team/TeamMemberTest.java
@@ -0,0 +1,159 @@
+package star.e2e.team;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import java.time.OffsetDateTime;
+import java.util.Collections;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import star.common.model.vo.Jido;
+import star.common.model.vo.Marker;
+import star.common.security.encryption.jwt.JwtManager;
+import star.member.dto.MemberInfoDTO;
+import star.member.service.MemberService;
+import star.team.dto.request.CreateTeamRequest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TeamMemberTest {
+
+  @LocalServerPort
+  private int port;
+
+  @Autowired
+  private MemberService memberService;
+
+  @Autowired
+  private JwtManager jwtManager;
+
+  private String leaderAccessToken;
+  private String memberAccessToken;
+  private Long teamId;
+
+  @BeforeEach
+  void setUp() {
+    RestAssured.port = port;
+    MemberInfoDTO leaderInfo = memberService.getMemberById(1L);
+    MemberInfoDTO memberInfo = memberService.getMemberById(2L);
+
+    leaderAccessToken = jwtManager.generateToken(leaderInfo);
+    memberAccessToken = jwtManager.generateToken(memberInfo);
+
+    Marker marker = new Marker(37.0, 127.0, "테스트 주소", "테스트 장소");
+    Jido jido = new Jido(marker, 10);
+    CreateTeamRequest createTeamRequestBody = CreateTeamRequest.builder()
+        .name("멤버 테스트 팀")
+        .description("멤버 테스트 팀 설명")
+        .whenToMeet(OffsetDateTime.now().plusDays(1))
+        .location(jido)
+        .maxParticipantCount(5)
+        .imageUrls(Collections.singletonList("member_test_image_url"))
+        .build();
+    teamId = createTeamAndGetId(createTeamRequestBody);
+  }
+
+  private Long createTeamAndGetId(CreateTeamRequest request) {
+    Response response = given()
+        .header("Authorization", "Bearer " + leaderAccessToken)
+        .contentType(ContentType.JSON)
+        .body(request)
+        .when()
+        .post("/groups");
+
+    response.then().statusCode(201);
+
+    String location = response.getHeader("Location");
+    return Long.valueOf(location.substring(location.lastIndexOf("/") + 1));
+  }
+
+  @Test
+  @DisplayName("팀 멤버 참여 및 탈퇴 테스트")
+  void joinAndLeaveTeamTest() {
+    // 팀 참여
+    given()
+        .log().all()
+        .header("Authorization", "Bearer " + memberAccessToken)
+        .contentType(ContentType.JSON)
+        .when()
+        .post("/groups/" + teamId + "/members")
+        .then()
+        .log().all()
+        .statusCode(204);
+
+    given()
+        .log().all()
+        .header("Authorization", "Bearer " + leaderAccessToken)
+        .contentType(ContentType.JSON)
+        .when()
+        .get("/groups/" + teamId)
+        .then()
+        .log().all()
+        .statusCode(200)
+        .body("nowParticipants", equalTo(2));
+
+    // 팀 탈퇴
+    given()
+        .log().all()
+        .header("Authorization", "Bearer " + memberAccessToken)
+        .contentType(ContentType.JSON)
+        .when()
+        .delete("/groups/" + teamId + "/members")
+        .then()
+        .log().all()
+        .statusCode(204);
+
+    given()
+        .log().all()
+        .header("Authorization", "Bearer " + leaderAccessToken)
+        .contentType(ContentType.JSON)
+        .when()
+        .get("/groups/" + teamId)
+        .then()
+        .log().all()
+        .statusCode(200)
+        .body("nowParticipants", equalTo(1));
+  }
+
+  @Test
+  @DisplayName("팀 멤버 목록 조회 테스트")
+  void getTeamMembersTest() {
+    // 멤버 추가
+    given()
+        .header("Authorization", "Bearer " + memberAccessToken)
+        .contentType(ContentType.JSON)
+        .when()
+        .post("/groups/" + teamId + "/members")
+        .then()
+        .statusCode(204);
+
+    // 멤버 목록 조회
+    given()
+        .log().all()
+        .header("Authorization", "Bearer " + leaderAccessToken)
+        .contentType(ContentType.JSON)
+        .when()
+        .get("/groups/" + teamId + "/members")
+        .then()
+        .log().all()
+        .statusCode(200)
+        .body("teamMembers", hasSize(2));
+
+    // 멤버 목록 조회 (토큰 없이)
+    given()
+            .log().all()
+            .contentType(ContentType.JSON)
+            .when()
+            .get("/groups/" + teamId + "/members")
+            .then()
+            .log().all()
+            .statusCode(401);
+  }
+}
