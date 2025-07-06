2ee071b54a82755dc951330819530d16fa3b73c6
rdme0 <we6610@gmail.com>
2025-07-06 18:02:27 +0900

    :fire: remove(vo): 필요 없는 import 문 제거

--- src/main/java/star/team/model/vo/EncryptedPassword.java
+++ src/main/java/star/team/model/vo/EncryptedPassword.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.util.regex.Pattern;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
