68286f430bed44b9b626739867d7a4e534c1fccb
rdme0 <we6610@gmail.com>
2025-07-08 00:00:37 +0900

    :sparkles: feat: 모든 team id 가져오는 메서드 추가

--- src/main/java/star/team/repository/TeamRepository.java
+++ src/main/java/star/team/repository/TeamRepository.java
@@ -1,9 +1,15 @@
 package star.team.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import star.team.model.entity.Team;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long>, TeamRepositoryCustom {
+
+    @Query("SELECT t.id FROM Team t")
+    List<Long> getAllTeamIds();
+
 }
