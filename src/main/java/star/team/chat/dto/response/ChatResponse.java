aed5f048eee8158fa36df222e05343e6eeb897c0
rdme0 <we6610@gmail.com>
2025-07-08 00:03:47 +0900

    :sparkles: feat: 채팅 응답 dto redis id 추가

--- src/main/java/star/team/chat/dto/response/ChatResponse.java
+++ src/main/java/star/team/chat/dto/response/ChatResponse.java
@@ -1,21 +1,28 @@
 package star.team.chat.dto.response;
 
+import jakarta.annotation.Nullable;
 import java.time.OffsetDateTime;
 import lombok.Builder;
 import star.common.util.CommonTimeUtils;
 import star.member.dto.MemberInfoDTO;
 import star.team.chat.dto.ChatDTO;
-import star.team.chat.model.entity.Chat;
 
 @Builder
 public record ChatResponse(
-        Long chatId, Long memberId, String email, OffsetDateTime chattedAt, String message
+        @Nullable
+        Long chatDbId,
+
+        @Nullable
+        Long chatRedisId,
+
+        Long memberId, String email, OffsetDateTime chattedAt, String message
 ) {
     public static ChatResponse from(ChatDTO chat) {
         MemberInfoDTO memberInfo = chat.memberInfo();
 
         return ChatResponse.builder()
-                .chatId(chat.chatId())
+                .chatDbId(chat.chatDbId())
+                .chatRedisId(chat.chatRedisId())
                 .memberId(memberInfo.id())
                 .email(memberInfo.email().getValue())
                 .chattedAt(CommonTimeUtils.convertLocalDateTimeToOffsetDateTime(chat.chattedAt()))
