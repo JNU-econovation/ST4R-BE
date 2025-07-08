33c2b7909f3f948ef39689c4c1338317e5fb9d6d
rdme0 <we6610@gmail.com>
2025-07-08 19:10:26 +0900

    :recycle: refactor: redis config dev/prod 로 리팩토링

--- src/main/java/star/common/infra/redis/config/RedisProdConfig.java
+++ src/main/java/star/common/infra/redis/config/RedisProdConfig.java
@@ -1,19 +1,19 @@
 package star.common.infra.redis.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
-@RequiredArgsConstructor
+@Profile("prod")
 @EnableConfigurationProperties(RedisProperties.class)
-@Profile("dev")
-public class RedisDevConfig {
+@RequiredArgsConstructor
+public class RedisProdConfig {
 
     private final RedisProperties redisProperties;
 
@@ -30,5 +30,4 @@ public class RedisDevConfig {
 
         return new LettuceConnectionFactory(config);
     }
-
 }
