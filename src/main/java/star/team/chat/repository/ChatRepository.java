11106fad79317ca1f0acf4d36f07c99bd9972b00
rdme0 <we6610@gmail.com>
2025-07-08 18:48:57 +0900

    :sparkles: feat(Repository): 안 읽은 채팅 수 조회 메서드 추가

--- src/main/java/star/team/chat/repository/ChatRepository.java
+++ src/main/java/star/team/chat/repository/ChatRepository.java
@@ -2,6 +2,7 @@ package star.team.chat.repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.Set;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,7 @@ import star.team.chat.model.entity.Chat;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+
     Page<Chat> getChatsByTeamMemberTeamId(Long teamId, Pageable pageable);
 
     void deleteChatsByTeamMemberTeamId(Long teamId);
@@ -17,4 +19,9 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
     Optional<Chat> getChatById(Long id);
 
     boolean existsByIdAndChattedAtBefore(Long id, LocalDateTime readTime);
+
+    long countByTeamMemberTeamIdAndChattedAtAfter(Long teamId, LocalDateTime lastReadAt);
+
+    long countByTeamMemberTeamIdAndChattedAtAfterAndRedisIdNotIn(Long teamId,
+            LocalDateTime lastReadAt, Set<Long> redisIds);
 }
