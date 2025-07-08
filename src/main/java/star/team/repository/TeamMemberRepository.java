888bd859a4471f8ce2f1c1fa53cc8c8179f7831a
rdme0 <we6610@gmail.com>
2025-07-08 22:22:21 +0900

    :sparkles: feat: 내가 속한 팀 가져오는 기능 추가

--- src/main/java/star/team/repository/TeamMemberRepository.java
+++ src/main/java/star/team/repository/TeamMemberRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import star.team.model.entity.Team;
 import star.team.model.entity.TeamMember;
 
 @Repository
@@ -25,4 +26,7 @@ public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
 
     @Query("SELECT tm.team.id FROM TeamMember tm WHERE tm.member.id = :memberId")
     List<Long> getTeamIdsByMemberId(Long memberId);
+
+    @Query("SELECT tm.team FROM TeamMember tm WHERE tm.member.id = :memberId")
+    List<Team> getTeamsByMemberId(Long memberId);
 }
