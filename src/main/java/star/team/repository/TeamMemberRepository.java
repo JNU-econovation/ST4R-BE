502c38ac5e9f4fa228f1683f89658b3d5066f363
rdme0 <we6610@gmail.com>
2025-07-08 16:17:30 +0900

    :sparkles: feat: 한 팀의 모든 회원 id 가져오는 메서드 추가

--- src/main/java/star/team/repository/TeamMemberRepository.java
+++ src/main/java/star/team/repository/TeamMemberRepository.java
@@ -2,6 +2,7 @@ package star.team.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import star.team.model.entity.TeamMember;
 
@@ -19,4 +20,7 @@ public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
     boolean existsByTeamIdAndMemberId(Long teamId, Long memberId);
 
     List<TeamMember> getByTeamIdAndIsBanned(Long teamId, boolean b);
+
+    @Query("SELECT tm.member.id FROM TeamMember tm WHERE tm.team.id = :teamId")
+    List<Long> getAllMemberIdInTeam(Long teamId);
 }
