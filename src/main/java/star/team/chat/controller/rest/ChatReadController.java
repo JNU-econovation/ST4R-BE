8a19d2fa52b1ec3e748d00b0a754359a6907e0cb
rdme0 <we6610@gmail.com>
2025-07-08 18:49:59 +0900

    :recycle: refactor: WebSocket -> Http

--- src/main/java/star/team/chat/controller/rest/ChatReadController.java
+++ src/main/java/star/team/chat/controller/rest/ChatReadController.java
@@ -1,5 +1,6 @@
 package star.team.chat.controller.rest;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -7,22 +8,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import star.common.annotation.ResolvePageable;
 import star.common.constants.SortField;
 import star.common.security.dto.StarUserDetails;
 import star.team.chat.dto.response.ChatReadResponse;
+import star.team.chat.dto.response.UnreadChatCountsResponse;
 import star.team.chat.service.ChatCoordinateService;
 
 @RestController
-@RequestMapping("groups/{teamId}/chats/readCounts")
+@RequestMapping("/groups")
 @RequiredArgsConstructor
 public class ChatReadController {
 
     private final ChatCoordinateService service;
 
-    @GetMapping
+    @GetMapping("/chats/unreadCounts")
+    public ResponseEntity<List<UnreadChatCountsResponse>> getUnreadCounts(
+            @AuthenticationPrincipal StarUserDetails userDetails
+    ) {
+        return ResponseEntity.ok(service.getUnreadChatCounts(userDetails.getMemberInfoDTO()));
+    }
+
+    @GetMapping("/{teamId}/chats/readCounts")
     public ResponseEntity<Slice<ChatReadResponse>> getReadCounts(
             @PathVariable Long teamId,
             @AuthenticationPrincipal StarUserDetails userDetails,
@@ -33,4 +43,13 @@ public class ChatReadController {
         );
     }
 
+    @PostMapping("/{teamId}/chats/markAsRead")
+    public ResponseEntity<Void> markAsRead(
+            @PathVariable Long teamId,
+            @AuthenticationPrincipal StarUserDetails userDetails
+    ) {
+        service.markAsRead(teamId, userDetails.getMemberInfoDTO());
+        return ResponseEntity.noContent().build();
+    }
 }
+
