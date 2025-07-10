0da7dc299fd9a18482f274c44a1d3898c3f84718
rdme0 <we6610@gmail.com>
2025-07-10 21:50:57 +0900

    :technologist: feat: DevInitializer 구현

---
+++ src/main/java/star/common/initializer/DevInitializer.java
@@ -0,0 +1,28 @@
+package star.common.initializer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import star.common.security.encryption.jwt.JwtManager;
+import star.member.dto.MemberInfoDTO;
+import star.member.service.MemberService;
+
+@Profile("dev")
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class DevInitializer {
+
+    private final MemberService memberService;
+    private final JwtManager jwtManager;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        MemberInfoDTO admin = memberService.getMemberById(1L);
+        String token = jwtManager.generateToken(admin);
+        log.info("관리자 토큰: {}", token);
+    }
+}
