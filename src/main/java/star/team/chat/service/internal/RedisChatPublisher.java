1e95d4990986d553e0565605d8440909c789bac8
rdme0 <we6610@gmail.com>
2025-07-11 00:33:08 +0900

    :sparkles: feat: preview Publish 추가

--- src/main/java/star/team/chat/service/internal/RedisChatPublisher.java
+++ src/main/java/star/team/chat/service/internal/RedisChatPublisher.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
+import star.team.chat.dto.response.ChatPreviewResponse;
 import star.team.chat.dto.response.ChatResponse;
 
 @Service
@@ -11,8 +12,13 @@ import star.team.chat.dto.response.ChatResponse;
 public class RedisChatPublisher {
 
     private final RedisTemplate<String, ChatResponse> chatRedisTemplate;
+    private final RedisTemplate<String, ChatPreviewResponse> chatPreviewRedisTemplate;
 
     public void publishChat(ChannelTopic topic, ChatResponse chatResponse) {
         chatRedisTemplate.convertAndSend(topic.getTopic(), chatResponse);
+    }
+
+    public void publishChatPreview(ChannelTopic topic, ChatPreviewResponse chatPreviewResponse) {
+        chatPreviewRedisTemplate.convertAndSend(topic.getTopic(), chatPreviewResponse);
     }
 }
