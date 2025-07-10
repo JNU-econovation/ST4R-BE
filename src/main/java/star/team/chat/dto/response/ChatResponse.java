1df3347a1310cae6afa8e83aa4b69171a6349592
rdme0 <we6610@gmail.com>
2025-07-11 01:20:49 +0900

    :fire: remove: Nullable 제거

--- src/main/java/star/team/chat/dto/response/ChatResponse.java
+++ src/main/java/star/team/chat/dto/response/ChatResponse.java
@@ -1,6 +1,5 @@
 package star.team.chat.dto.response;
 
-import jakarta.annotation.Nullable;
 import java.time.OffsetDateTime;
 import lombok.Builder;
 import star.common.util.CommonTimeUtils;
@@ -9,13 +8,12 @@ import star.team.chat.dto.ChatDTO;
 
 @Builder
 public record ChatResponse(
-
         Long teamId,
-
-        @Nullable
         Long chatId,
-
-        Long memberId, String email, OffsetDateTime chattedAt, String message
+        Long memberId,
+        String email,
+        OffsetDateTime chattedAt,
+        String message
 ) {
     public static ChatResponse from(ChatDTO chat) {
         MemberInfoDTO memberInfo = chat.memberInfo();
