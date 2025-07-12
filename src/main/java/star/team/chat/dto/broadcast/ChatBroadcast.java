49f032a310a77726e84715b051dc5c402ef96270
rdme0 <we6610@gmail.com>
2025-07-12 16:31:24 +0900

    :truck: rename: 필드, 메서드 이름 변경

--- src/main/java/star/team/chat/dto/broadcast/ChatBroadcast.java
+++ src/main/java/star/team/chat/dto/broadcast/ChatBroadcast.java
@@ -7,13 +7,13 @@ import star.team.chat.enums.MessageType;
 @Builder
 public record ChatBroadcast(
         String messageType,
-        ChatMessage chatMessage
+        ChatMessage message
 ) {
     public static ChatBroadcast from(MessageType messageType, ChatMessage message) {
 
         return ChatBroadcast.builder()
                 .messageType(messageType.getTypeString())
-                .chatMessage(message)
+                .message(message)
                 .build();
     }
 }
