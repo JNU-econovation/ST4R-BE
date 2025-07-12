a7492b74ed61eddf2c77d8394e4af1bd9672ac25
rdme0 <we6610@gmail.com>
2025-07-12 14:51:57 +0900

    :sparkles: feat: toAuthor 메서드 추가

--- src/main/java/star/member/dto/MemberInfoDTO.java
+++ src/main/java/star/member/dto/MemberInfoDTO.java
@@ -1,6 +1,7 @@
 package star.member.dto;
 
 import lombok.Builder;
+import star.common.dto.internal.Author;
 import star.member.model.entity.Member;
 import star.member.model.entity.Role;
 import star.member.model.vo.Email;
@@ -14,6 +15,14 @@ public record MemberInfoDTO(Long id, Email email, Role role, String profileImage
                 .email(member.getEmail())
                 .role(member.getRole())
                 .profileImageUrl(member.getProfileImageUrl())
+                .build();
+    }
+
+    public Author toAuthor() {
+        return Author.builder()
+                .id(this.id())
+                .nickname(this.email().getValue())
+                .imageUrl(this.profileImageUrl())
                 .build();
     }
 
