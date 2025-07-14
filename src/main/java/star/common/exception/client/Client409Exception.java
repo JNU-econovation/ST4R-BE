8cb0bd19c717db3d2ad61e3a96f04194bd89fc7f
rdme0 <we6610@gmail.com>
2025-07-15 04:17:58 +0900

    :fire: remove: 필요 없는 예외 제거

--- src/main/java/star/common/exception/client/Client409Exception.java
+++
@@ -1,8 +0,0 @@
-package star.common.exception.client;
-
-public class Client409Exception extends ClientException {
-
-    public Client409Exception(String message) {
-        super(message);
-    }
-}
