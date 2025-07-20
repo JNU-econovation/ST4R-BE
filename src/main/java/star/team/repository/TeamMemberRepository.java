b30ba85d72c0dc5d2006baab825fd1c7327ea36f
rdme0 <we6610@gmail.com>
2025-07-20 20:09:50 +0900

    :bug: fix: soft delete 적용 안되는 버그 수정

--- src/main/java/star/team/repository/TeamMemberRepository.java
+++ src/main/java/star/team/repository/TeamMemberRepository.java
@@ -42,7 +42,7 @@ public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
 
     List<TeamMember> getByTeamIdAndIsBanned(Long teamId, boolean b);
 
-    @Query("SELECT tm.member.id FROM TeamMember tm WHERE tm.team.id = :teamId")
+    @Query("SELECT tm.member.id FROM TeamMember tm WHERE tm.team.id = :teamId AND tm.isDeprecated = false AND tm.isBanned = false")
     List<Long> getAllMemberIdInTeam(Long teamId);
 
     @Query("SELECT tm.team.id FROM TeamMember tm WHERE tm.member.id = :memberId AND tm.isDeprecated = false AND tm.isBanned = false")
