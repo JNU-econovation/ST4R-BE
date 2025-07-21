bf7f1cc4c247b280f16f44c45523e850d8baddbd
rdme0 <we6610@gmail.com>
2025-07-21 20:20:17 +0900

    :rocket: deploy: nginx & certbot 충돌 문제 해결

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,46 @@
 #!/bin/sh
+set -e
 
-# 'envsubst'는 템플릿 파일 내의 $변수들을 실제 환경 변수 값으로 치환해줍니다.
-envsubst '${BACKEND_SERVER_NAME}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
+CONF_PATH="/etc/nginx/nginx.conf"
+TEMPLATE_PATH="/etc/nginx/templates/nginx.conf.template"
+CERT_PATH="/etc/letsencrypt/live/${BACKEND_SERVER_NAME}/fullchain.pem"
 
-# Nginx를 foreground로 실행하여 컨테이너가 종료되지 않도록 합니다.
+HTTPS_BLOCK=$(cat <<EOF
+server {
+  listen 443 ssl http2;
+  server_name ${BACKEND_SERVER_NAME};
+
+  ssl_certificate /etc/letsencrypt/live/${BACKEND_SERVER_NAME}/fullchain.pem;
+  ssl_certificate_key /etc/letsencrypt/live/${BACKEND_SERVER_NAME}/privkey.pem;
+  ssl_protocols TLSv1.2 TLSv1.3;
+  ssl_prefer_server_ciphers on;
+
+  location / {
+    proxy_pass http://spring:8080;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header Host \$http_host;
+  }
+}
+EOF
+)
+
+if [ -f "$CERT_PATH" ]; then
+  echo "✅ 인증서 발견, HTTPS 서버 블록 삽입"
+  awk -v block="$HTTPS_BLOCK" '
+    {
+      if ($0 ~ /### HTTPS_SERVERS ###/) {
+        print block
+      } else {
+        print
+      }
+    }
+  ' "$TEMPLATE_PATH" > "$CONF_PATH"
+else
+  echo "❗ 인증서 없음, HTTPS 서버 블록 미삽입"
+  awk '
+    $0 !~ /### HTTPS_SERVERS ###/ { print }
+  ' "$TEMPLATE_PATH" > "$CONF_PATH"
+fi
+
 exec nginx -g 'daemon off;'
