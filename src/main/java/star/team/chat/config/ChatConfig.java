7fdba8261b474b9dbaf331a4bd4b6825c028d00b
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-07-24 13:30:09 +0900

    :lock: feat: 채팅 cors 추가

--- src/main/java/star/team/chat/config/ChatConfig.java
+++ src/main/java/star/team/chat/config/ChatConfig.java
@@ -7,6 +7,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import star.common.security.config.AllowedOriginsProperties;
 import star.common.security.interceptor.WebSocketJwtAuthInterceptor;
 import star.common.security.interceptor.WebSocketSecurityInterceptor;
 
@@ -17,11 +18,14 @@ public class ChatConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSocketJwtAuthInterceptor jwtAuthInterceptor;
     private final WebSocketSecurityInterceptor securityInterceptor;
+    private final AllowedOriginsProperties originsProperties;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket/connect")
-                .setAllowedOriginPatterns("http://localhost:*", "https://localhost:*")
+                .setAllowedOriginPatterns(
+                        originsProperties.getAllowedFeRedirectOrigins().toArray(new String[0])
+                )
                 .withSockJS();
     }
 
